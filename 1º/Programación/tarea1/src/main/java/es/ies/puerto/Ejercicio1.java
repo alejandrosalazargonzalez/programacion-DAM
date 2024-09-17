ec4c8a072f209f12e26d8e6ce90a0da54189e7ea
Salazar <115075962+alejandrosalazargonzalez@users.noreply.github.com>
2024-09-17 17:16:29 +0100

    Add files via upload

---
+++ 1º/Programación/tarea1/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -0,0 +1,17 @@
+package es.ies.puerto;
+
+/**
+ * Declara una variable entera para almacenar tu edad y muestra 
+ * su valor por pantalla.
+ * @author alejandrosalazargonzalez
+ * @version 1.0.0 160924
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        int edad = 20;
+
+        System.out.println("Tengo: " + edad + " años");
+
+    }
+}
