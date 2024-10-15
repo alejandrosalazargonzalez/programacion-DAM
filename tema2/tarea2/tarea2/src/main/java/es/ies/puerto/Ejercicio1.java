4d2c3d5c1f5bfb956b19e8c455bdadde0b286fd1
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2024-10-15 19:22:47 +0100

    se sube lo hecho en la clase del 15/10/2024

--- tema2/tarea2/tarea2/src/main/java/es/ies/puerto/Ejercicio1.java
+++ tema2/tarea2/tarea2/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -1,10 +1,13 @@
 package es.ies.puerto;
-
+    
 public class Ejercicio1 {
     
     private static Persona persona;
 
     public static void main(String[] args) {
-        persona = new Persona();
+        
+        persona = new Persona("Juan", 20);
+        persona.mostrarInformacion();
     }
+
 }
