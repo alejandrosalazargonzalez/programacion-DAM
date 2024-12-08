ab870d7801f8672d1e6680e1595bb74dbb35e21a
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2024-12-08 22:45:59 +0000

    ya casi

--- tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio14.java
+++ tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio14.java
@@ -1,5 +1,8 @@
 package es.ies.puerto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,13 +10,32 @@ import java.util.List;
  *  @version: 1.0.0
  */
 public class Ejercicio14 {
+    static List<Integer> lista = new ArrayList<>(Arrays.asList(1,2,5,3,1,4,7,5,6,7,8));
 
     public static void main(String[] args) {
+        System.out.println(lista);
+        System.out.println(encontrarDuplicados(lista));
 
     }
 
-    List<Integer> encontrarDuplicados(List<Integer> lista)
+    /**
+     * Metodo que ordena una lista para averiguar si hay algun valor duplicado
+     * y retorna una nueva lista con los valores duplicados
+     * @param lista
+     * @return
+     */
+    static List<Integer> encontrarDuplicados(List<Integer> lista)
     {
-        return lista;
+        List<Integer> duplicados = new ArrayList<>();
+        Collections.sort(lista);
+        
+        for (int i = 1; i < lista.size(); i++)
+        {
+            if (lista.get(i).equals(lista.get(i - 1)))
+            {
+                duplicados.add(lista.get(i));
+            }
+        }
+        return duplicados;
     }
 }
--- tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio15.java
+++ tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio15.java
@@ -1,8 +1,45 @@
 package es.ies.puerto;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ *  @author: alejandrosalazargonzalez
+ *  @version: 1.0.0
+ */
 public class Ejercicio15 {
-public static void main(String[] args) {
-            }
+    static List<Integer> lista = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+    public static void main(String[] args) {
+        System.out.println(lista);
+        System.out.println(rotarLista(lista, 11));
+
+    }
+
+    /**
+     * Metodo que rota la lista la cantidad de posiciones indicadas
+     * @param lista
+     * @param posiciones
+     * @return
+     */
+    static List<Integer> rotarLista(List<Integer> lista, int posiciones)
+    {
+        if (lista == null || lista.isEmpty())
+        {
+            return lista;
+        }
+        posiciones %= lista.size();
+        List<Integer> resultado = new ArrayList<>();
+        for (int i = lista.size() - posiciones; i < lista.size(); i++)
+        {
+            resultado.add(lista.get(i));
+        }
+        for (int i = 0; i < lista.size() - posiciones; i++)
+        {
+            resultado.add(lista.get(i));
+        }
+
+        return resultado;
+    }
 }
