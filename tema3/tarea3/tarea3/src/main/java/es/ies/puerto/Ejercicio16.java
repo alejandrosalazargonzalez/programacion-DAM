1e2470b005d875536efefcc7401e3891de630d56
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2024-12-09 15:00:39 +0000

    ya casi 16 y 17 hechos

--- tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio16.java
+++ tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio16.java
@@ -1,8 +1,36 @@
+
 package es.ies.puerto;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ *  @author: alejandrosalazargonzalez
+ *  @version: 1.0.0
+ */
 public class Ejercicio16 {
-public static void main(String[] args) {
-            }
+    static List<Integer> lista = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+    public static void main(String[] args) {
+        System.out.println(lista);
+        System.out.println(intercambiar(lista, 0, 5));
+    }
+
+    static List<Integer> intercambiar(List<Integer> lista, int indice1, int indice2)
+    {
+        if (lista == null || lista.isEmpty())
+        {
+            return lista;
+        }
+        if (indice1 > lista.size() || indice1 < 0 || indice2 > lista.size() || indice2 < 0 )
+        {
+            return lista;
+        }
+
+        Integer auxiliar = lista.get(indice1);
+        lista.set(indice1, lista.get(indice2));
+        lista.set(indice2, auxiliar);
+        return lista;
+    }
 }
--- tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio17.java
+++ tema3/tarea3/tarea3/src/main/java/es/ies/puerto/Ejercicio17.java
@@ -1,8 +1,43 @@
+
 package es.ies.puerto;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ *  @author: alejandrosalazargonzalez
+ *  @version: 1.0.0
+ */
 public class Ejercicio17 {
-public static void main(String[] args) {
+    static List<Integer> lista = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3, 4));
+
+    public static void main(String[] args) {
+        System.out.println(lista);
+        System.out.println(eliminarDuplicados(lista));
+    }
+
+    /**
+     * Metodo que elimina los elementos duplicados en la lista
+     * @param lista
+     * @return
+     */
+    static List<Integer> eliminarDuplicados(List<Integer> lista)
+    {
+        if (lista == null || lista.isEmpty())
+        {
+            return lista;
+        }
+        for (int i = 1; i < lista.size(); i++)
+        {
+            for (int j = 1 + i; j < lista.size(); j++)
+            {
+                if (lista.get(j).equals(lista.get(i)))
+                {
+                    lista.remove(j);
+                }
             }
+        }
+        return lista;
+    }
 }
