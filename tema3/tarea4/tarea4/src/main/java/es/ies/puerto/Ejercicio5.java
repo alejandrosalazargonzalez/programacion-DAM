64f5276bc945f783768a36b24f0345dcc90e2d3f
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2024-12-10 20:28:43 +0000

    solo queda uno

--- tema3/tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio5.java
+++ tema3/tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio5.java
@@ -2,6 +2,7 @@ package es.ies.puerto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import java.util.Vector;
@@ -13,17 +14,24 @@ import java.util.Vector;
 public class Ejercicio5 {
     static List<Integer> enteros = new ArrayList<>(Arrays.asList(4, 2, 5, 1, 3));
     static Vector<Integer> vector = new Vector<>(enteros); 
- 
+
     public static void main(String[] args) {
-
+        System.out.println(vector);
+        ordenarVector(vector);
+        System.out.println(vector);
     }
-
+    /**
+     * metodo para ordenar el vector 
+     * @param vector
+     * @return
+     */
     static boolean ordenarVector(Vector<Integer> vector)
     {
         if (vector.isEmpty() || vector == null)
         {
             return false;
         }
+        Collections.sort(vector);
         return true;
     }
     
--- tema3/tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio8.java
+++ tema3/tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio8.java
@@ -1,7 +1,50 @@
 package es.ies.puerto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *  @author: alejandrosalazargonzalez
+ *  @version: 1.0.0
+ */
 public class Ejercicio8 {
+    static List<String> paises = new ArrayList<>(Arrays.asList("Italia", "Chipre", "Argentina", "España"));
+    static Vector<String> vectorPaises = new Vector<>(paises); 
+    static List<String> colores = new ArrayList<>(Arrays.asList("Azul", "Amarillo", "Verde", "Anaranjado"));
+    static Vector<String> vectorColores = new Vector<>(colores);
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(vectorPaises);
+        System.out.println(vectorColores);
+        System.out.println(fusionarVectores(vectorColores,vectorPaises));
+    }
+
+    /**
+     * metodo que fusiona dos vectores
+     * @param vector1
+     * @param vector2
+     * @return
+     */
+    static Vector<String> fusionarVectores(Vector<String> vector1, Vector<String> vector2)
+    {
+        if (vector1 == null || vector1.isEmpty())
+        {
+            return vector1;
+        }
+        if(vector2 == null || vector2.isEmpty())
+        {
+            return vector2;
+        }
+        Vector<String> vectorCopia = new Vector<>(vector1);
+        for (String nombre : vector2)
+        {
+            if (!vectorCopia.contains(nombre))
+            {
+                vectorCopia.add(nombre);
+            }
+        }
+        return vectorCopia;
     }
 }
