ebb8740b009eaf776baf5e428338fa67e6fcc09b
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2025-01-20 18:49:47 +0000

    ej2 tarea2 tema 4 mejorado

--- tema4/tarea2/logica-fechas/logica-fechas/src/main/java/es/ies/puerto/Ejercicio2.java
+++ tema4/tarea2/logica-fechas/logica-fechas/src/main/java/es/ies/puerto/Ejercicio2.java
@@ -15,21 +15,22 @@ public class Ejercicio2 {
         {
             return -1;
         }
-        LocalDate localDate = LocalDate.of(anio, mes.getValue(), 0);
-        int numeroMes = localDate.getMonthValue();
-        
+        LocalDate localDate = LocalDate.of(anio, mes.getValue(), 1);
+        return localDate.lengthOfMonth();
+        /*
         switch (numeroMes) {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 return 31;
             case 2:
                 if (localDate.isLeapYear()) {
-                    return 28;
+                    return 29;
                 }
-                return 27;
+                return 28;
             case 4: case 6: case 9: case 11:
                 return 30;
             default:
                 return -1;
         }
+                 */
     }
 }
