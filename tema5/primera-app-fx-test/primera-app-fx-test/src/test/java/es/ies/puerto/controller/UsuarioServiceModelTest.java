f3a4260e28f80ea759ee593a91a9a4fcbb4a5ab3
salazar <alejandrosalazargonzalez2004@gmail.com>
2025-04-21 16:18:10 +0100

    clase del 21/4

---
+++ tema5/primera-app-fx-test/primera-app-fx-test/src/test/java/es/ies/puerto/controller/UsuarioServiceModelTest.java
@@ -0,0 +1,45 @@
+package es.ies.puerto.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Properties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import es.ies.puerto.config.ConfigManager;
+import es.ies.puerto.model.UsuarioEntity;
+import es.ies.puerto.model.UsuarioServiceModel;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+@ExtendWith(MockitoExtension.class)
+class UsuarioServiceModelTest {
+
+    @Mock
+    private UsuarioServiceModel usuarioServiceModelMock;
+
+
+    @BeforeEach
+    void setUp() {
+        //usuarioServiceModel = new UsuarioServiceModel();
+        //when(metodo).thenReturn(valor);
+        System.out.println("Valor de UsuarioServiceModel: "+usuarioServiceModelMock);
+    }
+
+    @Test
+    void openUsuarioPorEmailTest(){
+        when(usuarioServiceModelMock.obtenerUsuarioPorEmail(""));
+    }
+    
+}
